fd37a18f23b51837d5d14e9c32560ff3f5b74b2c
Mohammad Ibrahim <iibrahim01081998@gmail.com>
2022-10-05 02:46:21 +0600

    header add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
+
 
 const Header = () => {
     return (
-        <div>
-            <p>common header</p>
-            <Link to='/home'>Home</Link>
+        <div className=' bg-gray-500 '>
+          
+          <navbar className='font-serif ... space-x-5 '> 
+          <Link to='/home'>Home</Link>
             <Link to='/about'>About</Link>
             <Link to='/menu'>Menu</Link>
             <Link to='/order'>Order</Link>
             <Link to='/meals'>Meals</Link>
-   
+          </navbar>
         </div>
     );
 };
