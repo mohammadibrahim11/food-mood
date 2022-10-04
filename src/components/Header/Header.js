be6e0d525fca6662d0e8e494ca9cd0aac71cd3ae
Mohammad Ibrahim <iibrahim01081998@gmail.com>
2022-10-04 19:32:29 +0600

    commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <a href="/home" target="_blank" rel="noopener noreferrer">home</a>
+                <a href="/menu" target="_blank" rel="noopener noreferrer">menu</a>
+
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Menu = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h1>this is menu page</h1>
+        </div>
+    );
+};
+
+export default Menu;
